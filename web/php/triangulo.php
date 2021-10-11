0fcc0ff5e1bcedeb56c28bcf8652c320a38a02c3
DarKaRo <jalp_18@hotmail.com>
2021-10-11 16:50:59 -0400

    Finished 1st Activity

--- web/php/triangulo.php
+++ web/php/triangulo.php
@@ -2,17 +2,38 @@
 function calcular_hipotenusa($a,$b){
     return sqrt(pow($a,2) + pow($b,2));
 }
-echo "Hello"
+$lado_a = 3;
+$lado_b = 4;
+$unidad = "cm";
+$hipotenusa = calcular_hipotenusa($lado_a,$lado_b);
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/styles.css">
-    <title>Triangulo</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Cálculo de Hipotenusa</title>
+    <header>
+        <h1>Cálculo de hipotenusa</h1>
+        <h2>Johandry López - 29714201</h2>
+    </header>
+    <div class="data">
+        <p>Lado a: <?php echo $lado_a?></p>
+        <div class="separator"></div>
+        <p>Lado b: <?php echo $lado_b?></p>
+        <div class="separator"></div>
+        <p>Valor de la hipotenusa: <?php echo $hipotenusa?></p>
+        <div class="separator"></div>
+    </div>
+    <nav class="activities row center">
+        <a href="../index.html">Regresar</a>
+        <a href="./php/triangulo.php">GitHub</a>
+     </nav>
 </head>
-
 <body>
+</html>
